ac496efa1a65b9affd3c792eb79e84c0b0e6dd3b
Aathithya Jegatheesan <87900917+Aathithya-J@users.noreply.github.com>
2022-12-04 11:30:43 +0800

    Update DAY2_P2.cpp

--- DAY2_P2.cpp
+++ DAY2_P2.cpp
@@ -1,1 +1,44 @@
+#include <iostream>
+using namespace std;
+char Comp, Player;
+long int add, sum;
+int main() {
+  cout << "";
+  for (int i = 0; i < 2500; i++) {
+    cin >> Comp >> Player;
+    if (Comp == 'A' && Player == 'X') {
+      add = 3;
+      sum += add;
+    } else if (Comp == 'A' && Player == 'Y') {
+      add = 4;
+      sum += add;
+    } else if (Comp == 'A' && Player == 'Z') {
+      add = 8;
+      sum += add;
+    }
+
+    if (Comp == 'B' && Player == 'X') {
+      add = 1;
+      sum += add;
+    } else if (Comp == 'B' && Player == 'Y') {
+      add = 5;
+      sum += add;
+    } else if (Comp == 'B' && Player == 'Z') {
+      add = 9;
+      sum += add;
+    }
+
+    if (Comp == 'C' && Player == 'X') {
+      add = 2;
+      sum += add;
+    } else if (Comp == 'C' && Player == 'Y') {
+      add = 6;
+      sum += add;
+    } else if (Comp == 'C' && Player == 'Z') {
+      add = 7;
+      sum += add;
+    }
+  }
+  cout << sum;
+}
 
