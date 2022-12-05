22e2be69905adade952a2250799dd0290c684518
Aathithya Jegatheesan <87900917+Aathithya-J@users.noreply.github.com>
2022-12-05 14:58:11 +0800

    Update Task001.cpp

--- ProjectEuler/Task001.cpp
+++ ProjectEuler/Task001.cpp
@@ -1,1 +1,15 @@
-
+#include <iostream>
+using namespace std;
+long int sum;
+int main() {
+  for (int i = 0; i < 1000; i++) {
+    if (i % 3 == 0) {
+      sum += i;
+    } else if (i % 5 == 0) {
+      sum += i;
+    } else if (i % 5 == 0 and i % 3 == 0) {
+      sum += i;
+    }
+  }
+  cout << sum;
+}
