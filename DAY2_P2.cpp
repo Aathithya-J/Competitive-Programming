be078a8b47c8270a48ea789d46d59aa57b1f679b
Aathithya Jegatheesan <87900917+Aathithya-J@users.noreply.github.com>
2022-12-05 10:46:54 +0800

    Update DAY2_P2.cpp

--- DAY2_P2.cpp
+++ DAY2_P2.cpp
@@ -1,44 +1,45 @@
+#include <fstream>
 #include <iostream>
+#include <string>
 using namespace std;
-char Comp, Player;
+ifstream data("input.txt");
+string line;
 long int add, sum;
 int main() {
   cout << "";
-  for (int i = 0; i < 2500; i++) {
-    cin >> Comp >> Player;
-    if (Comp == 'A' && Player == 'X') {
+  while (getline(data, line)) {
+    if (line == "A X") {
       add = 3;
       sum += add;
-    } else if (Comp == 'A' && Player == 'Y') {
+    } else if (line == "A Y") {
       add = 4;
       sum += add;
-    } else if (Comp == 'A' && Player == 'Z') {
+    } else if (line == "A Z") {
       add = 8;
       sum += add;
     }
 
-    if (Comp == 'B' && Player == 'X') {
+    if (line == "B X") {
       add = 1;
       sum += add;
-    } else if (Comp == 'B' && Player == 'Y') {
+    } else if (line == "B Y") {
       add = 5;
       sum += add;
-    } else if (Comp == 'B' && Player == 'Z') {
+    } else if (line == "B Z") {
       add = 9;
       sum += add;
     }
 
-    if (Comp == 'C' && Player == 'X') {
+    if (line == "C X") {
       add = 2;
       sum += add;
-    } else if (Comp == 'C' && Player == 'Y') {
-      add = 6;
+    } else if (line == "C Y") {
+      add = 2;
       sum += add;
-    } else if (Comp == 'C' && Player == 'Z') {
+    } else if (line == "C Z") {
       add = 7;
       sum += add;
     }
   }
   cout << sum;
 }
-
