fcb327ccf9ddb9f498c4f386d3929a29d8c9cce7
Aathithya Jegatheesan <87900917+Aathithya-J@users.noreply.github.com>
2022-12-05 10:40:42 +0800

    Create DAY1_P1.cpp

---
+++ DAY1_P1.cpp
@@ -0,0 +1,18 @@
+#include <fstream>
+#include <iostream>
+#include <string>
+using namespace std;
+ifstream data("input.txt");
+string mytext;
+long int add, sum, bigt;
+int main() {
+  while (getline(data, mytext)) {
+    if (mytext==""){
+
+    }else{
+      add=stoi(mytext);
+      sum+=add
+;    }
+  }
+  cout<<bigt;
+}
