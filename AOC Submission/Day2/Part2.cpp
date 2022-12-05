79a999d9b5950f91ffba35567f3748147f840e5c
Aathithya Jegatheesan <87900917+Aathithya-J@users.noreply.github.com>
2022-12-05 14:12:58 +0800

    Create Part2.cpp

---
+++ AOC Submission/Day2/Part2.cpp
@@ -0,0 +1,45 @@
+#include <fstream>
+#include <iostream>
+#include <string>
+using namespace std;
+ifstream data("input.txt");
+string line;
+long int add, sum;
+int main() {
+  cout << "";
+  while (getline(data, line)) {
+    if (line == "A X") {
+      add = 4;
+      sum += add;
+    } else if (line == "A Y") {
+      add = 8;
+      sum += add;
+    } else if (line == "A Z") {
+      add = 3;
+      sum += add;
+    }
+
+    if (line == "B X") {
+      add = 1;
+      sum += add;
+    } else if (line == "B Y") {
+      add = 5;
+      sum += add;
+    } else if (line == "B Z") {
+      add = 9;
+      sum += add;
+    }
+
+    if (line == "C X") {
+      add = 7;
+      sum += add;
+    } else if (line == "C Y") {
+      add = 2;
+      sum += add;
+    } else if (line == "C Z") {
+      add = 6;
+      sum += add;
+    }
+  }
+  cout << sum;
+}
