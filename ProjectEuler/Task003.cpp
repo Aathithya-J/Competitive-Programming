a95020178dc86fd1bad1f23bd4ad2e431710746d
Aathithya Jegatheesan <87900917+Aathithya-J@users.noreply.github.com>
2022-12-05 14:23:53 +0800

    Create Task003.cpp

---
+++ ProjectEuler/Task003.cpp
@@ -0,0 +1,33 @@
+#include <bits/stdc++.h>
+#include <iostream>
+using namespace std;
+long int maxPrimeFactors(long int n) {
+  long int maxPrime = -1;
+  while (n % 2 == 0) {
+    maxPrime = 2;
+    n >>= 1;
+  }
+  while (n % 3 == 0) {
+    maxPrime = 3;
+    n = n / 3;
+  }
+  for (int i = 5; i <= sqrt(n); i += 6) {
+    while (n % i == 0) {
+      maxPrime = i;
+      n = n / i;
+    }
+    while (n % (i + 2) == 0) {
+      maxPrime = i + 2;
+      n = n / (i + 2);
+    }
+  }
+  if (n > 4)
+    maxPrime = n;
+  return maxPrime;
+}
+
+int main() {
+  long int n;
+  n = 600851475143;
+  cout << maxPrimeFactors(n);
+}
