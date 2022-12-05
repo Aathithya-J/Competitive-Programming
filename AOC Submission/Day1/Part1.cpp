d0148bf6b9743cfe98f243bcbc75595539b19936
Aathithya Jegatheesan <87900917+Aathithya-J@users.noreply.github.com>
2022-12-05 14:09:57 +0800

    Update Part1.cpp

--- AOC Submission/Day1/Part1.cpp
+++ AOC Submission/Day1/Part1.cpp
@@ -1,1 +1,19 @@
+#include <fstream>
+#include <iostream>
+#include <string>
+using namespace std;
+ifstream data("input.txt");
+string mytext;
+long int add, sum, bigt;
+int main() {
+  while (getline(data, mytext)) {
+    if (mytext==""){
+
+    }else{
+      add=stoi(mytext);
+      sum+=add
+;    }
+  }
+  cout<<bigt;
+}
 
