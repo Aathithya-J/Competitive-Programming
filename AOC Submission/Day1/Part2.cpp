d14801c394b94c1cf70d2d5df0f62c8d237507ad
Aathithya Jegatheesan <87900917+Aathithya-J@users.noreply.github.com>
2022-12-05 14:10:44 +0800

    Update Part2.cpp

--- AOC Submission/Day1/Part2.cpp
+++ AOC Submission/Day1/Part2.cpp
@@ -1,1 +1,24 @@
+#include <bits/stdc++.h>
+#include <fstream>
+#include <iostream>
+#include <string>
+using namespace std;
+ifstream data("input.txt");
+string mytext;
 
+long int n, add, sum, bigt;
+int main() {
+  vector<int> g1;
+  while (getline(data, mytext)) {
+    if (mytext == "") {
+      g1.push_back(sum);
+      sum = 0;
+    } else {
+      add = stoi(mytext);
+      sum += add;
+    }
+  }
+  sort(g1.begin(), g1.end(), greater<int>());
+
+  cout << g1[0] + g1[1] + g1[2];
+}
