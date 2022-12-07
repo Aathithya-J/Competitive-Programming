722a339162bad1f468a0896ab89d7f393331afbc
Aathithya Jegatheesan <87900917+Aathithya-J@users.noreply.github.com>
2022-12-07 08:55:17 +0800

    Update Part2.cpp

--- AOC Submission/Day3/Part2.cpp
+++ AOC Submission/Day3/Part2.cpp
@@ -4,35 +4,40 @@
 using namespace std;
 int main() {
   vector<string> allstr = {};
+  vector<char> finalint = {};
   ifstream data("input.txt");
   string content;
-  string sb1,sb2,sb3;
-  int a,u,sum,k,c = 0, d = c+1, e = c+2;
+  string sb1, sb2, sb3;
+  int a, u, sum=0;
+  int c = 0;
+  int d = 1;
+  int e = 2;
+  int value = 0;
   while (getline(data, content)) {
     allstr.push_back(content);
   }
-  while (k < allstr.size()) {
+  while (e < allstr.size()) {
     sb1 = allstr[c];
     sb2 = allstr[d];
     sb3 = allstr[e];
-    for (int c = 0; e < sb1.size(); ++c) {
+    for (int c = 0; c < sb1.size(); ++c) {
       for (int d = 0; d < sb2.size(); ++d) {
         for (int e = 0; e < sb3.size(); ++e) {
           if (sb1[c] == sb2[d] && sb1[c] == sb3[e]) {
-            a=sb1[c];
+            a = sb1[c];
           }
         }
       }
+      if (a < 96) {
+        u = a - 38;
+      } else {
+        u = a - 96;
+      }
     }
-    if (a < 96) {
-      u = a - 38;
-    } else {
-      u = a - 96;
-    }
-    sum += u;
     c += 3;
     d += 3;
     e += 3;
+    sum += u;
   }
-  cout << sum ;
+  cout << sum;
 }
