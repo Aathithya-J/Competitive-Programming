5fde731223880000c00c60b5f49ae359366ef0c4
Aathithya Jegatheesan <87900917+Aathithya-J@users.noreply.github.com>
2022-12-07 15:55:05 +0800

    Update Part1.cpp

--- AOC Submission/Day6/Part1.cpp
+++ AOC Submission/Day6/Part1.cpp
@@ -1,33 +1,34 @@
 #include <fstream>
 #include <iostream>
+#include <set>
 #include <string>
 #include <vector>
 using namespace std;
 ifstream data("input.txt");
-int a, w, x, y, z, letters, count;
-string line, s;
-int cont;
+int a, b, count;
+int letters = 14;
+string line, c;
+set<char> total;
+vector<string> finalvals;
 int main() {
-  letters=4;
-  w = 0;
-  x = 1;
-  y = 2;
-  z = 3;
-  while (getline(data, line)) {
-    a = line.length();
-    for (int i = 0; i < a; i++) {
-      if (line[w] != line[x] && line[w] != line[y] && line[w] != line[z] &&
-          line[x] != line[y] && line[x] != line[z] && line[y] != line[z]) {
-        cout << letters;
-        return 0;
-      } else {
-        letters += 1;
-        w += 1;
-        x += 1;
-        y += 1;
-        z += 1;
-      }
+  a = 0;
+  getline(data, line);
+  c = line.substr(a, 4);
+  cout << c << endl;
+  while (c.length() >= 4) {
+    for (int i = 0; i < c.length(); i++) {
+      total.insert(c[i]);
     }
+    if (total.size() == 4) {
+      cout << "Finals: " << c << endl;
+      cout << "" << letters;
+      return 0;
+    } else {
+      total.clear();
+      a += 1;
+      letters += 1;
+    }
+    c = line.substr(a, 1);
+    cout << c << endl;
   }
-  cout << letters;
 }
