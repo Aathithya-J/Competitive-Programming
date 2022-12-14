b68325464e49a49a14abcaedf3b041fd52ac5b31
Aathithya Jegatheesan <87900917+Aathithya-J@users.noreply.github.com>
2022-12-14 11:42:49 +0800

    Update Remove duplicates.cpp

--- Algorithm & Problem Solving Practice 14.12/Remove duplicates.cpp
+++ Algorithm & Problem Solving Practice 14.12/Remove duplicates.cpp
@@ -1,26 +1,24 @@
 #include <bits/stdc++.h>
 using namespace std;
-void removeDuplicates(int arr[], int n)
-{
-	int i;
-	set<int> s;
-	for (i = 0; i < n; i++) {
-		s.insert(arr[i]);
-	}
-	set<int>::iterator it;
-	cout << "\nAfter removing duplicates:\n";
-	for (it = s.begin(); it != s.end(); ++it)
-		cout << *it << ", ";
-	cout << '\n';
+void removeDuplicates(int arr[], int n) {
+  int i;
+  set<int> s;
+  for (i = 0; i < n; i++) {
+    s.insert(arr[i]);
+  }
+  set<int>::iterator it;
+  cout << "\nAfter removing duplicates:\n";
+  for (it = s.begin(); it != s.end(); ++it)
+    cout << *it << ", ";
+  cout << '\n';
 }
-int main()
-{
-	int arr[] = { 4, 2, 3, 3, 2, 4 };
-	int n = sizeof(arr) / sizeof(arr[0]);
-	cout << "\nBefore removing duplicates:\n";
-	for (int i = 0; i < n; i++)
-		cout << arr[i] << " ";
-	removeDuplicates(arr, n);
-	return 0;
+int main() {
+  int arr[] = {4, 2, 3, 3, 2, 4};
+  int n = sizeof(arr) / sizeof(arr[0]);
+  cout << "\nBefore removing duplicates:\n";
+  for (int i = 0; i < n; i++)
+    cout << arr[i] << " ";
+  removeDuplicates(arr, n);
+  return 0;
 }
 //This code was referenced from other sources
