afa22490b2b13a83ccc6cf1c6994e93f0a8c0cb7
Aathithya Jegatheesan <87900917+Aathithya-J@users.noreply.github.com>
2022-12-21 16:42:44 +0800

    Create Chef and Brain Speed.cpp

---
+++ CodeChef/Chef and Brain Speed.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int cb, bb;
+	cin>>cb>>bb;
+	if (cb>=bb){
+	    cout<<"NO"<<endl;
+	}else{
+	    cout<<"Yes"<<endl;
+	}
+	return 0;
+}
