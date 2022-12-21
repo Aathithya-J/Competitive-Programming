e7ba70511b4781dd2feebe8cd1b4155ba314e2ae
Aathithya Jegatheesan <87900917+Aathithya-J@users.noreply.github.com>
2022-12-21 16:54:46 +0800

    Create (6) Total Price Money.cpp

---
+++ CodeChef/(6) Total Price Money.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    long int c10,c90 ,n;
+    cin>>n;
+    for (int i=0; i<n; i++){
+	cin>>c10>>c90;
+	cout<<(c10*10+c90*90)<<endl;
+    }
+	return 0;
+}
