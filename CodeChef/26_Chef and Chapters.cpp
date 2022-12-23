a6fcbef42238cdbe6fc9595693285d8aa563f564
Aathithya Jegatheesan <87900917+Aathithya-J@users.noreply.github.com>
2022-12-23 09:28:10 +0800

    Create 26_Chef and Chapters.cpp

---
+++ CodeChef/26_Chef and Chapters.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	int line, X,Y,Z;
+	cin>>line;
+	for(int i=0;i<line;i++){
+	    cin>>X>>Y>>Z;
+	    cout<<(X*Y*Z)<<endl;
+	}
+	return 0;
+}
