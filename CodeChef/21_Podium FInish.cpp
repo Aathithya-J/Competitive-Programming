c908494f45155c772921f1414ef2699d4e6cbd2b
Aathithya Jegatheesan <87900917+Aathithya-J@users.noreply.github.com>
2022-12-23 09:18:40 +0800

    Create 21_Podium FInish.cpp

---
+++ CodeChef/21_Podium FInish.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	int line, t1,t2;
+	cin>>line;
+	for(int i=0;i<line;i++){
+	    cin>>t1>>t2;
+	    cout<<(t1+t2)<<endl;
+	    
+	}
+	return 0;
+}
