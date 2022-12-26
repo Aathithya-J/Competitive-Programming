9bfa596347c10bbec5f4c61a564e2bd38357f2dc
Aathithya Jegatheesan <87900917+Aathithya-J@users.noreply.github.com>
2022-12-26 16:26:32 +0800

    Create 63_AI Analysing Code.cpp

---
+++ CodeChef/63_AI Analysing Code.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int line,X;
+	    cin>>X;
+	    if (X<=1000){
+	        cout<<"Yes"<<endl;
+	    }else{
+	        cout<<"NO"<<endl;
+	    }
+    }
+
